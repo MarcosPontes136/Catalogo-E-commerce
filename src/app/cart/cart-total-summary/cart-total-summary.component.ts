b66398af668d80e06792538088bad637fd4556b5
MarcosPontes136 <zeuscod@outlook.com>
2025-01-10 17:30:40 -0300

    Incluido Paginação angular material.

--- src/app/cart/cart-total-summary/cart-total-summary.component.ts
+++ src/app/cart/cart-total-summary/cart-total-summary.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CartService } from '../../shared/services/cartService/cart.service';
 
 @Component({
   selector: 'app-cart-total-summary',
-  imports: [MatCardModule, MatButtonModule, AsyncPipe, CurrencyPipe],
+  imports: [MatCardModule, MatButtonModule, CurrencyPipe],
   templateUrl: './cart-total-summary.component.html',
   styleUrl: './cart-total-summary.component.scss'
 })
---
+++ src/app/shared/paginator/paginator.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import { JsonPipe } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-paginator',
+  imports: [MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    JsonPipe,
+  ],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.scss'
+})
+export class PaginatorComponent {
+    @Input() length = 0;
+    pageSize = 0;
+    pageIndex = 0;
+    pageSizeOptions = [4, 8, 15];
+
+    @Output() pageChange = new EventEmitter<{ pageSize: number; pageIndex: number }>();
+
+    showPageSizeOptions = true;
+
+    pageEvent!: PageEvent;
+
+    handlePageEvent(e: PageEvent) {
+      this.pageChange.emit({ pageSize: e.pageSize, pageIndex: e.pageIndex });
+    }
+
+    setPageSizeOptions(setPageSizeOptionsInput: string) {
+      if (setPageSizeOptionsInput) {
+        this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+      }
+    }
+
+}
